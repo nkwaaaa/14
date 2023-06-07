33ba7f153f3eed9d5b2ae39d249ce069d8105257
nkwaaaa <nkwaaaa@gmail.com>
2023-06-07 07:52:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Banner from "./components/banner/index";
+import Character from "./components/cards/index";
+
+
+export default function App() {
+  return (
+     <>
+      <Banner />
+      <Character />
+    </>
+  )
+}
